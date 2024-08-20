9c0c5e0b073894935a6629658b688d73f8ecd049
Risminator <vjdronov@yandex.ru>
2024-08-20 20:51:05 +0300

    BUILD

--- Assets/Contra/RunNGunScript.cs
+++ Assets/Contra/RunNGunScript.cs
@@ -2,7 +2,6 @@ using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditorInternal;
 using UnityEngine;
 
 public class RunNGunScript : MonoBehaviour
