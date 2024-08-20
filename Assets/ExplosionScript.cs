37724ffea53e90c1f2fddf2c61f60e9e769d2c4b
Risminator <vjdronov@yandex.ru>
2024-08-20 13:12:45 +0300

    meta files and explosion sound set-up

--- Assets/ExplosionScript.cs
+++ Assets/ExplosionScript.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class ExplosionScript : MonoBehaviour
 {
-
+    AudioManager audioManager;
     private void Awake()
     {
-        
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        if (audioManager != null)
+        {
+        }
+        
         // Звук 
     }
 
